48efb3a6cae990cf7fdc226f34661b0dbeb7dc2a
gass <gass@ubuntu-uwb.lan>
2017-05-19 17:14:38 +0800

    数据库建立初步

---
+++ dump.sql
@@ -0,0 +1,276 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: posdata
+-- ------------------------------------------------------
+-- Server version	5.7.18-0ubuntu0.17.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `posdata`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `posdata` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `posdata`;
+
+--
+-- Table structure for table `mobile38`
+--
+
+DROP TABLE IF EXISTS `mobile38`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mobile38` (
+  `idmobile38` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `position_x` int(11) DEFAULT NULL,
+  `position_y` int(11) DEFAULT NULL,
+  `position_z` int(11) DEFAULT NULL,
+  `last_heard` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `current_system_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idmobile38`)
+) ENGINE=InnoDB AUTO_INCREMENT=696 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mobile39`
+--
+
+DROP TABLE IF EXISTS `mobile39`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mobile39` (
+  `idmobile39` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `position_x` int(11) DEFAULT NULL,
+  `position_y` int(11) DEFAULT NULL,
+  `position_z` int(11) DEFAULT NULL,
+  `last_heard` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `current_system_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idmobile39`)
+) ENGINE=InnoDB AUTO_INCREMENT=696 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mobile40`
+--
+
+DROP TABLE IF EXISTS `mobile40`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mobile40` (
+  `idmobile40` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `position_x` int(11) DEFAULT NULL,
+  `position_y` int(11) DEFAULT NULL,
+  `position_z` int(11) DEFAULT NULL,
+  `last_heard` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `current_system_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idmobile40`)
+) ENGINE=InnoDB AUTO_INCREMENT=696 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mobile41`
+--
+
+DROP TABLE IF EXISTS `mobile41`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mobile41` (
+  `idmobile41` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `position_x` int(11) DEFAULT NULL,
+  `position_y` int(11) DEFAULT NULL,
+  `position_z` int(11) DEFAULT NULL,
+  `last_heard` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `current_system_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idmobile41`)
+) ENGINE=InnoDB AUTO_INCREMENT=696 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mobile42`
+--
+
+DROP TABLE IF EXISTS `mobile42`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mobile42` (
+  `idmobile42` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `position_x` int(11) DEFAULT NULL,
+  `position_y` int(11) DEFAULT NULL,
+  `position_z` int(11) DEFAULT NULL,
+  `last_heard` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `current_system_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idmobile42`)
+) ENGINE=InnoDB AUTO_INCREMENT=696 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pos_record_tb`
+--
+
+DROP TABLE IF EXISTS `pos_record_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pos_record_tb` (
+  `idpos_record_tb` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `position_x` int(11) DEFAULT NULL,
+  `position_y` int(11) DEFAULT NULL,
+  `position_z` int(11) DEFAULT NULL,
+  `mac_address` varchar(45) DEFAULT NULL,
+  `parent_mac_address` varchar(45) DEFAULT NULL,
+  `bridge_mac_address` varchar(45) DEFAULT NULL,
+  `sw_version` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `last_heard` datetime DEFAULT NULL,
+  `position_update_timestamp` datetime DEFAULT NULL,
+  `current_system_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idpos_record_tb`)
+) ENGINE=InnoDB AUTO_INCREMENT=55424 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_afterinsert_pos38 AFTER INSERT ON pos_record_tb FOR EACH ROW
+BEGIN
+     IF   new.name="mobile 38"  THEN
+     
+       insert into mobile38(id,name,position_x,position_y,position_z,last_heard,created_at,current_system_timestamp)  values(new.id, new.name , new.position_x , new.position_y, new.position_z , new.Last_heard , new.Created_at , new.Current_system_timestamp )   ;
+     
+     END IF;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_afterinsert_pos39 AFTER INSERT ON pos_record_tb FOR EACH ROW
+BEGIN
+     IF   new.name="mobile 39"  THEN
+     
+       insert into mobile39(id,name,position_x,position_y,position_z,last_heard,created_at,current_system_timestamp)  values(new.id, new.name , new.position_x , new.position_y, new.position_z , new.Last_heard , new.Created_at , new.Current_system_timestamp )   ;
+     
+     END IF;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_afterinsert_pos40 AFTER INSERT ON pos_record_tb FOR EACH ROW
+BEGIN
+     IF   new.name="mobile 40"  THEN
+     
+       insert into mobile40(id,name,position_x,position_y,position_z,last_heard,created_at,current_system_timestamp)  values(new.id, new.name , new.position_x , new.position_y, new.position_z , new.Last_heard , new.Created_at , new.Current_system_timestamp )   ;
+     
+     END IF;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_afterinsert_pos41 AFTER INSERT ON pos_record_tb FOR EACH ROW
+BEGIN
+     IF   new.name="mobile 41"  THEN
+     
+       insert into mobile41(id,name,position_x,position_y,position_z,last_heard,created_at,current_system_timestamp)  values(new.id, new.name , new.position_x , new.position_y, new.position_z , new.Last_heard , new.Created_at , new.Current_system_timestamp )   ;
+     
+     END IF;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER t_afterinsert_pos42 AFTER INSERT ON pos_record_tb FOR EACH ROW
+BEGIN
+     IF   new.name="mobile42"  THEN
+     
+       insert into mobile42(id,name,position_x,position_y,position_z,last_heard,created_at,current_system_timestamp)  values(new.id, new.name , new.position_x , new.position_y, new.position_z , new.Last_heard , new.Created_at , new.Current_system_timestamp )   ;
+     
+     END IF;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-19 17:12:38
